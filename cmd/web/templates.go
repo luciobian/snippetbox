8716ae359ca76161689d64935c0e3994201fd02f
Lucio Biancardi <lbiancardi91@gmail.com>
2024-03-03 18:22:59 -0300

    add data validation

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,12 +3,15 @@ package main
 import (
 	"html/template"
 	"lucio/snippetbox/pkg/models"
+	"net/url"
 	"path/filepath"
 	"time"
 )
 
 type templateData struct {
 	CurrentYear int
+	FormData    url.Values
+	FormErrors  map[string]string
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
