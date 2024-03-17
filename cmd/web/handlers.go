d98a5851860a7e5c3a8a87ff426b73e50e0caac0
Lucio Biancardi <lbiancardi91@gmail.com>
2024-03-17 14:10:43 -0300

    user signup and password encryption

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -75,11 +75,43 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) signUpUserForm(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display the user signup form...")
+	app.render(w, r, "signup.page.gohtml", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) signUpUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create a new user...")
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+	}
+	form := forms.New(r.PostForm)
+	form.Required("name", "email", "password")
+	form.MinLength("password", 10)
+	form.MatchesPattern("email", forms.EmailRX)
+
+	// if form not valid redisplay signup page
+	if !form.Valid() {
+		app.render(w, r, "signup.page.gohtml", &templateData{Form: form})
+		return
+	}
+
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+
+	if err == models.ErrDuplicateEmail {
+		form.Errors.Add("email", "Address is already in use.")
+		app.render(w, r, "signup.page.gohtml", &templateData{
+			Form: form,
+		})
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.session.Put(r, "flash", "Your signup was successful. Please log in.")
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
